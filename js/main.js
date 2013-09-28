68956a0e02dec0d199a409acc0376b619fbb1730
Jiasi Zeng <garthcn@gmail.com>
2013-09-28 05:49:37 -0700

    more

--- js/main.js
+++ js/main.js
@@ -9,5 +9,30 @@ var init = function () {
         if(e.keyName == "back")
             tizen.application.getCurrentApplication().exit();
     });
+    $('div#sub4').live('pageshow',function(event, ui){
+      console.log('p4 shown');
+      window.mySwipe = Swipe(document.getElementById('slider-project'));
+    });
+    $('div#sub4').live('pagehide',function(event, ui){
+      console.log('p4 hidden');
+      window.mySwipe = Swipe(document.getElementById('slider-project'));
+    });
+
+    $('.flip').click(function(){
+      if ($(this).find('.flipped').length == 0) {
+        $(this).find('.card').addClass('flipped');
+        console.log('click face');
+        return false;
+      }
+    });
+    $('body').on('click', '.flipped', function(evt) {
+      console.log('click flipped');
+      $(evt.currentTarget).removeClass('flipped');
+      return false;
+    });
 };
 $(document).ready(init);
+//$(document).one("mobileinit", function () {
+  //$.mobile.pageContainer = $('#container');
+  //$.mobile.defaultPageTransition = 'slide';
+//});
